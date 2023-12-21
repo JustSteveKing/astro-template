007bb35db162bcedc35c364356060d3259e26599
Steve McDougall <juststevemcd@gmail.com>
2023-12-21 14:43:20 +0000

    starting to build website configuration

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE: string = '';
+export const SITE_DESCRIPTION: string = '';
+
+export const TWITTER: string = '';
+export const GITHUB: string = '';
+export const LINKEDIN: string = '';
