dff33235602486f5797023bf2d34f1ccc4ab199c
Steve McDougall <juststevemcd@gmail.com>
2023-12-21 14:40:45 +0000

    Adding tailwindcss plugins

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,5 +4,9 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
+	],
 }
