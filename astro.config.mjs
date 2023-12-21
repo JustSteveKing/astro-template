8921cd0a89ced315a9c6bead5fdbaae3059be3d7
Steve McDougall <juststevemcd@gmail.com>
2023-12-21 14:39:10 +0000

    Enabling content collection cache in experimental flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    contentCollectionCache: true,
+  },
   integrations: [tailwind(), mdx()],
   prefetch: {
     defaultStrategy: 'viewport'
